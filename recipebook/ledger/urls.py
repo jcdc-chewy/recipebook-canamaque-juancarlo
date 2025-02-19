1530a9366aa1299ce124f5a994bd6ebdc6ff030e
jcdc-chewy <jcdc_chewy@proton.me>
2025-02-19 15:33:28 +0800

    [lab1] Added URLs that point to placeholder view functions in ledger

---
+++ recipebook/ledger/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('/recipes/list', views.page_one, name='page_one'),
+    path('/recipe/1', views.page_one, name='page_two'),
+    path('/recipe/2', views.page_one, name='page_three'),
+]
