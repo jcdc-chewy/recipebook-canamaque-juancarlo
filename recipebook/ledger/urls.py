09ad91eb6f242ad3d57ef607d1a4a2ff44932a3c
jcdc-chewy <jcdc_chewy@proton.me>
2025-02-19 16:00:08 +0800

    [lab1] Fixed minor errors in ledger app

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/recipes/list', views.page_one, name='page_one'),
-    path('/recipe/1', views.page_one, name='page_two'),
-    path('/recipe/2', views.page_one, name='page_three'),
+    path('recipes/list', views.page_one, name='page_one'),
+    path('recipe/1', views.page_two, name='page_two'),
+    path('recipe/2', views.page_three, name='page_three'),
 ]
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
 def page_one(request):
-    # ...
+    return HttpResponse('Test #1.')
 
 def page_two(request):
-    # ...
+    return HttpResponse('Test #2.')
 
 def page_three(request):
-    # ...
+    return HttpResponse('Test #3.')
