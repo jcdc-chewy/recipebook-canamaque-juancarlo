b11a1cbeb2dce09b8754a0910d24794ff97a199e
jcdc-chewy <jcdc_chewy@proton.me>
2025-03-07 22:59:28 +0800

    [lab2] Created an admin panel for each of the models

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from .models import Ingredient, Recipe, RecipeIngredient
 
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+ 
+    list_display = ('name',)
+    search_fields = ('name',)
 
-admin.site.register(Ingredient)
-admin.site.register(Recipe)
-admin.site.register(RecipeIngredient)
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    model = RecipeIngredient
+
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
