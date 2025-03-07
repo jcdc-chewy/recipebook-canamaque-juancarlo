3f6f87ed650cef02de803512e98a76972094b9bb
jcdc-chewy <jcdc_chewy@proton.me>
2025-03-07 22:17:22 +0800

    [lab2] Added ForeignKey relationships to RecipeIngredient model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -24,8 +24,8 @@ class Recipe(models.Model):
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=50)
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
 
     def __str__(self):
         return f"{self.quantity} of {self.ingredient.name} in {self.recipe.name}"
