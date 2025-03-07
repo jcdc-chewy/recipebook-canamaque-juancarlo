1a94d89738e7a029c54b88183a1a6b38598b828a
jcdc-chewy <jcdc_chewy@proton.me>
2025-03-07 21:28:38 +0800

    [lab2] Updated ledger/urls.py to handle dynamic URLs for each recipe

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('recipes/list', views.recipe_list, name='recipe_list'),
-    path('recipe/1', views.recipe_one, name='recipe_one'),
-    path('recipe/2', views.recipe_two, name='recipe_two'),
+    path('recipe/<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
 ]
