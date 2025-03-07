8854e774160c83c53a1a2b103efa11ac3c989529
jcdc-chewy <jcdc_chewy@proton.me>
2025-03-07 18:01:02 +0800

    [lab2] Initialized the 3 primary Django models for the project's database

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('ingredient_detail', args=[str(self.name)])
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('recipe_detail', args=[str(self.name)])
+
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=50)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.ingredient.name} in {self.recipe.name}"
