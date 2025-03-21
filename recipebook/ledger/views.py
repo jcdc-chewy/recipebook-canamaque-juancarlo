ac35c3bc519d45a5c45cf71839247e01e688fe8b
jcdc-chewy <jcdc_chewy@proton.me>
2025-03-21 17:32:10 +0800

    [lab3] Added @login_required to recipe_list and recipe_detail views to require authentication

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from .models import Recipe, RecipeIngredient
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, 'ledger/recipe-list.html', {'recipes': recipes})
 
-
+@login_required
 def recipe_detail(request, recipe_name):
     recipe = Recipe.objects.get(name=recipe_name)
     recipe_ingredients = RecipeIngredient.objects.filter(recipe=recipe)
