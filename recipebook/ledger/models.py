a595ce8d92caf525914b44656babf7314d91cfdc
jcdc-chewy <jcdc_chewy@proton.me>
2025-03-21 17:24:47 +0800

    [lab3] Added Author, CreatedOn, and UpdatedOn fields to Recipe model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -15,6 +15,9 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
