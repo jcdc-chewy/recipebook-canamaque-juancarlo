21e663b052b2d5df17317ca744ce50059f3dd0c9
jcdc-chewy <jcdc_chewy@proton.me>
2025-03-21 22:49:38 +0800

    [lab3] Set logout redirection to login page

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -118,8 +118,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 LOGIN_URL = '/accounts/login/'
+
 LOGIN_REDIRECT_URL = 'recipe_list'
+
+LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
