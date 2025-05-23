e0450f0fb915e6cc86ba2e1c4678a0ba8479b008
jcdc-chewy <jcdc_chewy@proton.me>
2025-05-23 20:31:11 +0800

    [lab4] Defined URL pattern for adding images for recipes

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('recipes/list', views.recipe_list, name='recipe_list'),
     path('recipe/<str:recipe_name>/', views.recipe_detail, name='recipe_detail'),
     path('recipe/add/', views.recipe_add, name='recipe_add'),
+    path('recipe/<int:pk>/add_image/', views.add_recipe_image, name='add_recipe_image'),
 ]
