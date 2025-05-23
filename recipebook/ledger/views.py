7d21c5484e9cb366d848ef8262f1ff28e99f7ee4
jcdc-chewy <jcdc_chewy@proton.me>
2025-05-23 19:02:46 +0800

    [lab4] Added related recipe images to recipe_detail view context

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -6,14 +6,18 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def recipe_list(request):
     recipes = Recipe.objects.all()
+
     return render(request, 'ledger/recipe-list.html', {'recipes': recipes})
 
 @login_required
 def recipe_detail(request, recipe_name):
     recipe = Recipe.objects.get(name=recipe_name)
     recipe_ingredients = RecipeIngredient.objects.filter(recipe=recipe)
+    recipe_images = recipe.images.all()
+
     return render(request, 'ledger/recipe.html', {
         'recipe': recipe,
         'recipe_ingredients': recipe_ingredients,
+        'recipe_images': recipe_images,
         'author': recipe.author,
     })
